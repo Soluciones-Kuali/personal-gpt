cbca7482edbe81eae1409cc4455d1cb633633350
Ivan Hernández <ivan2hdez@gmail.com>
2025-05-08 23:53:03 -0600

    Add temperature

--- src/app/services/chat.ts
+++ src/app/services/chat.ts
@@ -1,8 +1,14 @@
 import request from '@/utils/request';
 
-export function createChat(message: string) {
+export function createChat({
+  message,
+  temperature,
+}: {
+  message: string;
+  temperature: number;
+}) {
   return request('/api/chat', {
     method: 'POST',
-    body: { message },
+    body: { message, temperature },
   });
 }
