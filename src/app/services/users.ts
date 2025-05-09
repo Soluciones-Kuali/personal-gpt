8df23df40f17da7da535445303f2f64b857e369f
David Gibrán <david@solucioneskuali.com.mx>
2025-05-08 20:40:15 -0600

    remove unused imports and functions

--- src/app/services/users.ts
+++ src/app/services/users.ts
@@ -1,28 +1,5 @@
-import qs from 'qs';
-
-import { PaginatedResponse } from '@/types/pagination';
-
 import request from '@/utils/request';
 import { User, UserPayload } from '@/types/user';
-
-export function getUsers({
-  filter,
-  offset,
-  limit,
-}: {
-  filter: string;
-  offset: number;
-  limit: number;
-  sort?: string;
-}) {
-  return request<PaginatedResponse<User>>(
-    `/api/users?${qs.stringify({
-      filter,
-      offset,
-      limit,
-    })}`
-  );
-}
 
 export function createUser(product: UserPayload) {
   return request<User>('/api/users', {
